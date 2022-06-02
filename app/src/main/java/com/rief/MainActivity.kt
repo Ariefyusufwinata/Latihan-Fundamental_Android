e9c8e5d6ba9e9003f701f11c83c6776aefc6b2d0
Ariefyusufwinata <winata.arief26@gmail.com>
2022-06-02 19:26:01 +0700

    finish

--- app/src/main/java/com/rief/MainActivity.kt
+++ app/src/main/java/com/rief/MainActivity.kt
@@ -2,6 +2,8 @@ package com.rief
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
 import com.rief.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.apply {
+            btnChoose.setOnClickListener {
+                val checkedId = radioGroup.checkedRadioButtonId
+                var message : String = " "
+                when (checkedId) {
+                    R.id.cValueOne -> {
+                        message = "Value 1"
+                    }
+                    R.id.cValueTwo -> {
+                        message = "Value 2"
+                    }
+                    R.id.cValueThree -> {
+                        message = "Value 3"
+                    }
+                    -1 -> {
+                        message = "No Value"
+                    }
+                }
+                tvValue.text = message
+            }
+
+            btnReset.setOnClickListener {
+                radioGroup.clearCheck()
+            }
+
+            radioGroup.setOnCheckedChangeListener { _, checkedId ->
+                val rb = findViewById<RadioButton>(checkedId)
+                when (rb != null) {
+                    true -> Toast.makeText(this@MainActivity, "${rb.text}", Toast.LENGTH_SHORT).show()
+                    false -> Toast.makeText(this@MainActivity, "No Value", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
