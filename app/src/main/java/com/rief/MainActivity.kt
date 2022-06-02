73ee52664792a2a943fc3c23029814c4c1d5434f
Ariefyusufwinata <winata.arief26@gmail.com>
2022-06-02 16:19:49 +0700

    update gradle to 7.20

--- app/src/main/java/com/rief/MainActivity.kt
+++ app/src/main/java/com/rief/MainActivity.kt
@@ -2,10 +2,15 @@ package com.rief
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rief.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
